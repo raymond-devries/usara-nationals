4d40d8fc7d666c5b2036ad95cbdc1b4c15b45625
raymond-devries <raymond.l.devries@gmail.com>
2021-09-13 23:45:28 -0700

    Update raw data and finish cleaning data

--- clean_data.py
+++ clean_data.py
@@ -1,10 +1,30 @@
 import json
 import datetime
+from bs4 import BeautifulSoup
 
 
-def get_seconds(time: str):
+def parse_seconds(time: str):
     h, m, s = time.split(':')
-    return int(datetime.timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds())
+    return int(datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s)).total_seconds())
+
+
+def parse_checkpoint_data(raw_data: list):
+    data = raw_data[0::2]
+    data = [point for point in data if point != "&nbsp;"]
+    checkpoint_data = []
+    for point in data:
+        soup = BeautifulSoup(point, "html.parser")
+        stripped = list(soup.stripped_strings)
+        checkpoint_data.append(
+            {
+                "name": stripped[0][:-4],
+                "time": parse_seconds(stripped[2])
+            }
+        )
+
+    return checkpoint_data
+
+
 
 
 def extract_data_from_record(record):
@@ -14,10 +34,12 @@ def extract_data_from_record(record):
         "team_name": record[3],
         "team_type": record[5],
         "score": int(record[6]),
-        "finish_time": 0 if "disq" in record[7] else get_seconds(record[7]),
+        "finish_time": 0 if "disq" in record[7] else parse_seconds(record[7]),
         "points": int(record[8]),
         "bonus_penalty": 0 if record[9] == "&nbsp;" else int(record[9]),
-        "time_penalty": 0 if record[10] == "&nbsp;" else get_seconds(record[10])
+        "manual_adjustment": 0 if record[10] == "&nbsp;" else int(record[10]),
+        "time_penalty": 0 if record[11] == "&nbsp;" else parse_seconds(record[11]),
+        "checkpoints": parse_checkpoint_data(record[12:]),
     }
 
 
